e2515ac49d822c0487ce2f2df6c7d22be49c5f8c
Gregory Van Horne <54690769+gvanhorne@users.noreply.github.com>
2023-12-14 22:58:43 -0800

    Setup test module

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("part1");
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    input.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = include_str!("./input1.txt");
+        let result: String = part1(input);
+        assert_eq!(result, input);
+    }
 }
