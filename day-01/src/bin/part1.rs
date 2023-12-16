208ed34f3ef02294572b1d9a62a42ac0aacb3313
Gregory Van Horne <54690769+gvanhorne@users.noreply.github.com>
2023-12-16 12:17:15 -0800

    Get the last number of each line

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -9,6 +9,8 @@ fn main() {
 
 fn part1(input: &str) -> u32 {
     let mut first_numbers: Vec<u32> = Vec::new();
+    let mut last_numbers: Vec<u32> = Vec::new();
+
     // read each line
     match read_lines(input) {
         Ok(lines) => {
@@ -18,6 +20,9 @@ fn part1(input: &str) -> u32 {
                     if let Some(first_number) = extract_first_number(&ip) {
                         // Add the first number to the array
                         first_numbers.push(first_number);
+                    };
+                    if let Some(last_number) = extract_last_number(&ip) {
+                        last_numbers.push(last_number);
                     }
                 }
             }
@@ -27,7 +32,7 @@ fn part1(input: &str) -> u32 {
         }
     }
     // Return the sum of the array of first numbers
-    first_numbers.iter().sum()
+    last_numbers.iter().sum()
 }
 
 // The output is wrapped in a Result to allow matching on errors
@@ -42,6 +47,17 @@ where P: AsRef<Path>, {
 fn extract_first_number(s: &str) -> Option<u32> {
     // Find the position of the first digit in the string
     if let Some(index) = s.chars().position(|c| c.is_digit(10)) {
+        // Try to parse the substring as a u32
+        s.chars().nth(index).map(|c| c.to_digit(10)).flatten()
+    } else {
+        None // No digit found in the string
+    }
+}
+
+fn extract_last_number(s: &str) -> Option<u32> {
+    let char_vec: Vec<char> = s.chars().collect();
+    // Find the position of the last digit in the string
+    if let Some(index) = char_vec.iter().rposition(|c| c.is_digit(10)) {
         // Try to parse the substring as a u32
         s.chars().nth(index).map(|c| c.to_digit(10)).flatten()
     } else {
