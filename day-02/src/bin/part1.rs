ecea8ac190134fe20dcf58135f7d81deea7a2357
Gregory Van Horne <54690769+gvanhorne@users.noreply.github.com>
2023-12-17 00:35:32 -0800

    Define a game struct and invalid game fn

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -1,5 +1,32 @@
+struct Game {
+    id: u32,
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+fn build_game(game_info: &str) -> Game {
+    let mut game_iter = game_info.split(":");
+    let id = game_iter.next().map(|f| f.split(" ").nth(1)).unwrap().unwrap().parse::<u32>().unwrap();
+    let mut reveals_iter = game_iter.next().unwrap().split(";");
+    let first_game = reveals_iter.next();
+    let mut first_game_iter = first_game.unwrap().trim_start().split(" ");
+    let num_cubes = first_game_iter.next().unwrap().parse::<u32>();
+    let cube_colour = first_game_iter.next().unwrap().trim.parse::<String>();
+    dbg!(cube_colour);
+    // let cube_colour = first_game.unwrap().split(" ").next();
+    // let cube_colour = first_game.unwrap().next();
+    // reveals.unwrap().split(",");
+    Game {
+        id,
+        red: 2,
+        green: 3,
+        blue: 4,
+    }
+}
+
 fn process(input: &str) -> u32 {
-    let output: u32 = input.lines().flat_map(|line| {
+    let output: u32 = input.lines().filter(|&line| !invalid_game(line)).flat_map(|line| {
         line.chars().skip_while(|c| !c.is_digit(10))
             .take_while(|c| c.is_digit(10))
             .collect::<String>()
@@ -8,6 +35,11 @@ fn process(input: &str) -> u32 {
     }).sum();
 
     output
+}
+
+fn invalid_game(input: &str) -> bool {
+    build_game(input);
+    return true
 }
 
 fn main() {
