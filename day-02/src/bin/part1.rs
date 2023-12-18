9ea0c54357aaca9bb22f86ba3679e0a46b1b7660
Gregory Van Horne <54690769+gvanhorne@users.noreply.github.com>
2023-12-18 15:46:13 -0800

    Remove invalid games from output sum

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -1,32 +1,44 @@
 struct Game {
     id: u32,
-    red: u32,
-    green: u32,
-    blue: u32,
+    max_red: u32,
+    max_green: u32,
+    max_blue: u32,
 }
 
 fn build_game(game_info: &str) -> Game {
+    let mut max_red: u32 = u32::MIN;
+    let mut max_green: u32 = u32::MIN;
+    let mut max_blue: u32 = u32::MIN;
+
     let mut game_iter = game_info.split(":");
-    let id = game_iter.next().map(|f| f.split(" ").nth(1)).unwrap().unwrap().parse::<u32>().unwrap();
-    let mut reveals_iter = game_iter.next().unwrap().split(";");
-    let first_game = reveals_iter.next();
-    let mut first_game_iter = first_game.unwrap().trim_start().split(" ");
-    let num_cubes = first_game_iter.next().unwrap().parse::<u32>();
-    let cube_colour = first_game_iter.next().unwrap().trim.parse::<String>();
-    dbg!(cube_colour);
-    // let cube_colour = first_game.unwrap().split(" ").next();
-    // let cube_colour = first_game.unwrap().next();
-    // reveals.unwrap().split(",");
+    let id = game_iter.next().unwrap().split_whitespace().nth(1).unwrap().parse::<u32>().unwrap();
+    let elf_reveals = game_iter.next().unwrap().split(";");
+    for reveal in elf_reveals {
+        let colour_groups = reveal.trim_start().split(",");
+        for cubes in colour_groups {
+            let mut parts = cubes.split_whitespace();
+
+            let num_cubes: u32 = parts.next().unwrap().parse::<u32>().unwrap();
+            let colour: &str = parts.next().unwrap();
+            if (colour == "red") && (num_cubes > max_red) {
+                max_red = num_cubes;
+            } else if (colour == "green") && (num_cubes > max_green) {
+                max_green = num_cubes;
+            } else if (colour == "blue") && (num_cubes > max_blue) {
+                max_blue = num_cubes;
+            }
+        }
+    }
     Game {
         id,
-        red: 2,
-        green: 3,
-        blue: 4,
+        max_red,
+        max_green,
+        max_blue,
     }
 }
 
 fn process(input: &str) -> u32 {
-    let output: u32 = input.lines().filter(|&line| !invalid_game(line)).flat_map(|line| {
+    let output: u32 = input.lines().filter(|&line| !valid_game(line)).flat_map(|line| {
         line.chars().skip_while(|c| !c.is_digit(10))
             .take_while(|c| c.is_digit(10))
             .collect::<String>()
@@ -37,9 +49,12 @@ fn process(input: &str) -> u32 {
     output
 }
 
-fn invalid_game(input: &str) -> bool {
-    build_game(input);
-    return true
+fn valid_game(input: &str) -> bool {
+    let red_cubes: u32 = 12;
+    let green_cubes: u32 = 13;
+    let blue_cubes: u32 = 14;
+    let game = build_game(input);
+    return game.max_blue > blue_cubes || game.max_green > green_cubes || game.max_red > red_cubes;
 }
 
 fn main() {
