2359981c3515325c66de3e42418d4b7f0537c97c
Gregory Van Horne <54690769+gvanhorne@users.noreply.github.com>
2023-12-18 16:59:43 -0800

    Implement part 2

--- day-02/src/bin/part2.rs
+++ day-02/src/bin/part2.rs
@@ -1,14 +1,14 @@
 struct Game {
     id: u32,
-    max_red: u32,
-    max_green: u32,
-    max_blue: u32,
+    most_red: u32,
+    most_green: u32,
+    most_blue: u32,
 }
 
 fn build_game(game_info: &str) -> Game {
-    let mut max_red: u32 = u32::MIN;
-    let mut max_green: u32 = u32::MIN;
-    let mut max_blue: u32 = u32::MIN;
+    let mut most_red: u32 = u32::MIN;
+    let mut most_green: u32 = u32::MIN;
+    let mut most_blue: u32 = u32::MIN;
 
     let mut game_iter = game_info.split(":");
     let id = game_iter.next().unwrap().split_whitespace().nth(1).unwrap().parse::<u32>().unwrap();
@@ -20,41 +20,46 @@ fn build_game(game_info: &str) -> Game {
 
             let num_cubes: u32 = parts.next().unwrap().parse::<u32>().unwrap();
             let colour: &str = parts.next().unwrap();
-            if (colour == "red") && (num_cubes > max_red) {
-                max_red = num_cubes;
-            } else if (colour == "green") && (num_cubes > max_green) {
-                max_green = num_cubes;
-            } else if (colour == "blue") && (num_cubes > max_blue) {
-                max_blue = num_cubes;
+            if colour == "red" {
+                if num_cubes > most_red {
+                    most_red = num_cubes;
+                }
+            } else if colour == "green" {
+                if num_cubes > most_green {
+                    most_green = num_cubes;
+                }
+            } else if colour == "blue" {
+                if num_cubes > most_blue {
+                    most_blue = num_cubes;
+                }
             }
         }
     }
     Game {
         id,
-        max_red,
-        max_green,
-        max_blue,
+        most_red,
+        most_green,
+        most_blue,
     }
 }
 
 fn process(input: &str) -> u32 {
-    let output: u32 = input.lines().filter(|&line| !valid_game(line)).flat_map(|line| {
-        line.chars().skip_while(|c| !c.is_digit(10))
-            .take_while(|c| c.is_digit(10))
-            .collect::<String>()
-            .parse::<u32>()
-            .ok()
-    }).sum();
+    let output: u32 = input
+    .lines()
+    .filter(|&line| valid_game(line))
+    .map(|line| {
+        let game = build_game(line);
+        // dbg!(game.id, game.min_blue, game.min_red, game.min_green);
+        // println!("id: {}, red: {}, green: {}, blue: {}", game.id, game.most_red, game.most_green, game.most_blue);
+        game.most_blue * game.most_red * game.most_green
+    })
+    .sum();
 
     output
 }
 
 fn valid_game(input: &str) -> bool {
-    let red_cubes: u32 = 12;
-    let green_cubes: u32 = 13;
-    let blue_cubes: u32 = 14;
-    let game = build_game(input);
-    return game.max_blue > blue_cubes || game.max_green > green_cubes || game.max_red > red_cubes;
+    return true
 }
 
 fn main() {
@@ -71,13 +76,13 @@ mod tests {
     fn input1_works() {
         let file = include_str!("../../input1.txt");
         let result = process(file);
-        assert_eq!(result, 8);
+        assert_eq!(result, 2286);
     }
 
     #[test]
     fn input2_works() {
         let file = include_str!("../../input2.txt");
         let result = process(file);
-        assert_eq!(result, 2776);
+        assert_eq!(result, 68638);
     }
 }
